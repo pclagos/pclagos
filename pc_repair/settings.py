bd8935a1a5568c9d33f9589a0f2e9b284aaa369b
pclagos <contactopclagos@gmail.com>
2025-08-06 23:38:40 -0300

    Update settings.py

--- pc_repair/settings.py
+++ pc_repair/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&05t36p+^!-1tu9v806lq60xt=2v0mws#o19+8jg+9i0mpsmdo'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&05t36p+^!-1tu9v806lq60xt=2v0mws#o19+8jg+9i0mpsmdo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
